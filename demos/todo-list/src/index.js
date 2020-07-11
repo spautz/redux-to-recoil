a26fe05bccc206e49d814ae9e964981056a215c4
Steven Pautz <spautz@gmail.com>
2020-07-11 12:30:34 -0400

    Add Redux example project

--- demos/todo-list/src/index.js
+++ demos/todo-list/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(
+import rootReducer from './reducers';
+import App from './components/App';
+
+const store = createStore(rootReducer);
+
+render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
