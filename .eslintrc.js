23093c78fc587b22b2f39f466e31680ed17e0b19
Steven Pautz <spautz@gmail.com>
2020-07-29 23:05:52 -0400

    Cleanup and lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,18 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
+
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
 
   ignorePatterns: ['demos/', 'dist/', 'coverage/', 'node_modules/'],
 };
--- src/SyncReduxToRecoil.tsx
+++ src/SyncReduxToRecoil.tsx
@@ -3,8 +3,7 @@ import { Store } from 'redux';
 import { useSelector, useStore } from 'react-redux';
 import { useRecoilState } from 'recoil';
 
-import { internalStateAtom } from './internals';
-import { ReduxState } from './internals/types';
+import { ReduxState, internalStateAtom } from './internals';
 
 const selectEntireState = (state: ReduxState) => state;
 let store: Store | null = null;
@@ -23,6 +22,12 @@ const SyncReduxToRecoil: React.FC<SyncReduxToRecoilProps> = (props) => {
   const currentReduxState = useSelector(selectEntireState);
   if (enabled && currentReduxState !== lastReduxState) {
     setReduxState(currentReduxState);
+  }
+
+  if (__DEV__ && children) {
+    console.warn(
+      'Passing children to <SyncReduxToRecoil> is not recommended, as they will rerender on every Redux change',
+    );
   }
 
   return <React.Fragment>{children}</React.Fragment>;
