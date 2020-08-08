60b7d78adeea1d01be6d94d541018bf4b8e1cabd
Steven Pautz <spautz@gmail.com>
2020-08-08 08:32:43 -0400

    Updates

---
+++ precache-manifest.3581155161fab4ebb0b383241153a186.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced3d930d1fc2aae60f9df1198cf2fb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16f10ed3eda51dcf0a3a",
+    "url": "/static/js/2.21cf9972.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.21cf9972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503a0cf1c144602846b5",
+    "url": "/static/js/main.d51d3b80.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
