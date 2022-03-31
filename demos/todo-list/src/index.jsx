f7d549803c94515de1ec30f0c314bf7fb3f0df76
Steven Pautz <spautz@gmail.com>
2022-03-30 21:18:35 -0400

    Release v0.8.1 and update demo app (#62)
    * Update todo-list demo and validate against older dependencies
    
    * Validate against newer dependencies

--- demos/todo-list/src/index.jsx
+++ demos/todo-list/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
